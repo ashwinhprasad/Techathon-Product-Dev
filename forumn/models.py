fde32f933a74829df93e8a3fe7a54e552d297574
charann01 <61188435+charann01@users.noreply.github.com>
2021-12-18 08:36:25 +0530

    forumn schema

---
+++ forumn/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from user.models import UserModel
+
+# Create your models here.
+class QuestionsModel(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    asked_by = models.ForeignKey(UserModel,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title} - {self.id}"
+
+class AnswerModel(models.Model):
+    question = models.ForeignKey(QuestionsModel, on_delete=models.CASCADE)
+    answer = models.CharField()
+    answer_by = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.answer} - {self.id}"
