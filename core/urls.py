4cf10568f7f19668a204dfa13430749b672e1718
charann01 <61188435+charann01@users.noreply.github.com>
2021-12-18 10:03:14 +0530

    home url

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from user.views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('user.urls')),
-    path('forum/',include('forumn.urls'))
+    path('forum/',include('forumn.urls')),
+    path('home/',HomeView,name='home')
 ]
